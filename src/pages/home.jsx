3ba862b39e6422ce3f817438ff6fd0bcd6ae47e9
pratik19pipaliya <136100050+pratik19pipaliya@users.noreply.github.com>
2024-10-29 23:35:05 +0530

    update home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -625,7 +625,7 @@ function Home() {
             <div className="row py-2">
               <div className="col-12 text-center">
                 <p className="m-0 f-rob-med f-14 d-inline-block">
-                  For Third Party Manufacturing
+                  Pratik Lodu
                 </p>
                 <Link
                   to="/nutrition/white-labelling"
